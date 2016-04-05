2a6a1250fab58a57eef4d97482b43e9b058dfe2e
Jose Miguel <jmn8718@gmail.com>
2016-04-06 00:07:39 +0200

    added form

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,30 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    styles:[`
+        .error {
+            color: red;
+            font-size: 11px;
+        }
+    `],
+    template: `
+        <h1>My First Angular 2 App</h1>
+        <form #searchForm="ngForm">
+            <input type="text" required [(ngModel)]="model.search" ngControl="search" #inputSearch="ngForm">
+            <p class="error" [hidden]="inputSearch.valid">This input is required</p>
+            
+            <button type="submit" [disabled]="!searchForm.valid">SEND</button>
+        </form>
+    `,
+    directive: []
 })
-export class AppComponent { }
+export class AppComponent {
+    public model = {
+        search: ""
+    }
+
+    constructor(){
+
+    }
+
+}
